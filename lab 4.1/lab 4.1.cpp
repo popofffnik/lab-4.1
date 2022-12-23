e644b83229fe1123be8c697d3152b4199090c3f7
popofffnik <57560494+popofffnik@users.noreply.github.com>
2022-12-23 19:30:13 +0300

    Добавьте файлы проекта.

---
+++ lab 4.1/lab 4.1.cpp
@@ -0,0 +1,53 @@
+﻿#include <Windows.h>
+
+using namespace std;
+
+
+
+class Weapon
+
+{
+
+public:
+
+	string name;
+
+	int damage;
+
+	int weight;
+
+
+
+	Weapon(string name, int damage, int weight) :
+
+		name(name), damage(damage), weight(weight) {}
+
+
+
+	Weapon()
+
+	{
+
+		name = "Default Name";
+
+		damage = 1;
+
+		weight = 1;
+
+	}
+
+};
+
+int main()
+
+{
+
+	Weapon weapon;
+
+	cout << "object weapon - " << " " << weapon.name << " " << weapon.damage << " " << weapon.weight << endl;
+
+	Weapon sword("Sword", 5, 3);
+
+	cout << "object sword - " << " " << sword.name << " " << sword.damage << " " << sword.weight << endl;
+
+}
